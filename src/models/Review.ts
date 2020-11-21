a73740854e4cc90df06c6f334ac7f84e81e803f9
zahmis <meda829ratteru86@gmail.com>
2020-11-21 15:00:36 +0900

    fix

--- src/models/Review.ts
+++ src/models/Review.ts
@@ -1,8 +1,27 @@
 import {Description, Required} from "@tsed/schema";
-import {Model} from "@tsed/mongoose";
+import {Model, ObjectID, Ref} from "@tsed/mongoose";
+import {Reply} from "./Reply";
+import {User} from "./User";
+import { Sweet } from './Sweet';
+
 
 @Model()
 export class Review{
+    @ObjectID("id")
+    _id: string;
+
+    @Ref(User)
+    @Description("User ID")
+    userId: Ref<User>
+
+    @Ref(Sweet)
+    @Description("Sweet ID")
+    sweetId: Ref<User>
+
+    @Ref(Reply)
+    @Description("User ID")
+    replyId: Ref<Reply>
+
 
     @Description("Star")
     @Required()
@@ -10,5 +29,8 @@ export class Review{
 
     @Description("Comment")
     @Required()
+    
     comment: string;
+
+  
 }
