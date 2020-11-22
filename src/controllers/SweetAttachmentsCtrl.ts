3c5fc790d8e76b633c6884cd1cd1acc500189cff
zahmis <meda829ratteru86@gmail.com>
2020-11-22 15:11:47 +0900

    add api

---
+++ src/controllers/SweetAttachmentsCtrl.ts
@@ -0,0 +1,39 @@
+import {BodyParams, Controller, Get, PathParams, Post} from "@tsed/common";
+import {NotFound} from "@tsed/exceptions";
+import {Description, Required, Returns, Status, Summary} from "@tsed/schema";
+import {ReviewId} from "../decorators/ReviewId";
+import {SweetAttachment} from "../models/SweetAttachment";
+import { SweetAttachmentsService } from '../services/SweetAttachmentsService';
+
+@Controller({
+  path: "/attachments",
+})
+export class SweetAttachmentsCtrl {
+  constructor(private SweetAttachmentsService: SweetAttachmentsService) {}
+
+  @Get("/:id")
+  @Summary("Return a review by ID")
+  @(Status(200, SweetAttachment).Description("Success"))
+  async get(@PathParams("id") @ReviewId() id: string): Promise<SweetAttachment> {
+    const attachment = await this.SweetAttachmentsService.find(id);
+
+    if (attachment) {
+      return attachment;
+    }
+
+    throw new NotFound("attachment not found");
+  }
+
+  @Post("/")
+  @Summary("Create a new Review")
+  @(Returns(201, SweetAttachment).Description("Created"))
+  save(
+    @Description("SweetAttachment model")
+    @BodyParams()
+    @Required()
+    attachment: SweetAttachment
+  ) {
+    return this.SweetAttachmentsService.save(attachment);
+  }
+
+}
---
+++ src/services/SweetAttachmentsService.ts
@@ -0,0 +1,55 @@
+import {Inject, Service} from "@tsed/common";
+import {$log} from "@tsed/logger";
+import {MongooseModel} from "@tsed/mongoose";
+import {SweetAttachment} from "../models/SweetAttachment";
+
+@Service()
+export class SweetAttachmentsService {
+  @Inject(SweetAttachment)
+  private SweetAttachment: MongooseModel<SweetAttachment>;
+
+  $onInit() {
+    this.reload();
+  }
+
+  async reload() {
+    const attachments = await this.SweetAttachment.find({});
+
+    if (attachments.length === 0) {
+      const promises = require("../../resources/attachment.json").map((attachment: any) => this.save(attachment));
+      await Promise.all(promises);
+    }
+  }
+
+  /**
+   * Find a attachment by his ID.
+   * @param id
+   * @returns {undefined|SweetAttachment}
+   */
+  async find(id: string): Promise<SweetAttachment | null> {
+
+    const attachment = await this.SweetAttachment.findById(id).exec();
+
+    $log.debug("Found", attachment);
+
+    return attachment;
+  }
+
+  /**
+   *
+   * @param attachment
+   * @returns {Promise<TResult|TResult2|SweetAttachment>}
+   */
+  async save(attachment: SweetAttachment): Promise<SweetAttachment> {
+    $log.debug({message: "Validate review", attachment});
+
+    const model = new this.SweetAttachment(attachment);
+    $log.debug({message: "Save attachment", attachment});
+    await model.updateOne(attachment, {upsert: true});
+
+    $log.debug({message: "SweetAttachment saved", model});
+
+    return model;
+  }
+
+}
