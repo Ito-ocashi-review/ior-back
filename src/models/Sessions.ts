13b4d7cce61f6355847d12497cdfc8c6314cf775
itizawa <itizawa@weseek.co.jp>
2020-12-05 15:16:33 +0900

    create session model

---
+++ src/models/Sessions.ts
@@ -0,0 +1,14 @@
+import {Description, Required,Ref} from "@tsed/schema";
+import {Model} from "@tsed/mongoose";
+import { User } from './User';
+
+@Model()
+export class Session{
+
+  @Ref(User)
+  @Description("User ID")
+  userId: Ref<User>
+
+  @Description("Access Token")
+  accessToken: string;
+}
