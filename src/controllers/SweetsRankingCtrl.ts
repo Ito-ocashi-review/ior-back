5ee3aa31dfb1f2d198f6284ec1e4edb291299325
kntowd <kenta2231989@gmail.com>
2021-01-17 18:35:41 +0900

    rankingを作成するapiを作成

--- src/controllers/SweetsRankingCtrl.ts
+++ src/controllers/SweetsRankingCtrl.ts
@@ -6,7 +6,16 @@ import { ReviewsService} from "../services/ReviewsService";
 export class SweetsRankingCtrl {
   constructor(private SweetsService: SweetsService,private ReviewsService: ReviewsService) {}
   @Get("/")
-  async findAll() {
-    return await this.ReviewsService.getRanking();
+  async getRanking() {
+    // 後でランキングの計算方法を考えて実装
+    const sweetsRanking = [
+      {name: "kitkat",evaluation:4.7},
+      {name: "ぼたぼた焼き",evaluation:4.5},
+      {name: "じゃがりこ",evaluation:4.1},
+      {name: "うまい棒",evaluation:3.7},
+      {name: "ハイチュー",evaluation:2.7},
+      {name: "キャベツ太郎",evaluation:1.2},
+    ]
+    return sweetsRanking;
   }
 }
